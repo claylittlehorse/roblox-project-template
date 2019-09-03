347c747494c752ea6c498ab28cc70615667d9281
unknown <dog456pop@gmail.com>
2019-09-03 13:38:40 -0700

    Added LayoutProvider

--- src/UI/Root/App.lua
+++ src/UI/Root/App.lua
@@ -1,22 +1,18 @@
---[[
-	Entry-point to the game UI.
-]]
+-- Entrypoint to game UI
 
 local import = require(game.ReplicatedStorage.Lib.Import)
 
 local Roact = import "Roact"
 local RoactRodux = import "RoactRodux"
 
-local LayoutProvider = import "../LayoutProvider"
--- local LanguageProvider = import "../LanguageProvider"
+local LayoutProvider = import "UI/Root/LayoutProvider"
 
 local function App(store)
 	return Roact.createElement(RoactRodux.StoreProvider, { store = store }, {
 		Roact.createElement("ScreenGui", {
 			ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 		}, {
-			LayoutProvider = Roact.createElement(LayoutProvider),
-			-- LanguageProvider = Roact.createElement(LanguageProvider),
+			LayoutProvider = Roact.createElement(LayoutProvider)
 		})
 	})
 end
